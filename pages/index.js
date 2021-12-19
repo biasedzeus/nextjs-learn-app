ca48f2cc5dc4abe21e5c09ab19ac2dcbc797043d
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-19 21:00:18 +0530

    routing using Link Comp
    replace prop sends back to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-          <h1>Home</h1>  
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/products">
+        <a>products</a>
+      </Link>
+      <Link href="/docs">
+        <a>Docs</a>
+      </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- pages/products/[productId]/index.js
+++ pages/products/[productId]/index.js
@@ -1,9 +1,15 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function ProductDetail() {
   const router = useRouter();
   const pId = router.query.productId;
 
-  return <h1>Product details :{pId}</h1>;
+  return (
+      <div>
+      <h2><Link href='/products'><a>All Products</a></Link></h2>
+        <h1>Looking At product id : {pId}</h1>
+      </div>
+  );
 }
 export default ProductDetail;
