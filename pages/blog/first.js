b6a8dba33f8588bc3ae04588bfe9e3494a04b67b
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-19 19:23:52 +0530

    nested routes
    nested routes using folders and folder name works aas the route name

---
+++ pages/blog/first.js
@@ -0,0 +1,9 @@
+function First() {
+  return (
+    <div>
+      <h1>First Blog Page</h1>
+    </div>
+  );
+}
+
+export default First;
---
+++ pages/blog/second.js
@@ -0,0 +1,10 @@
+function Second() {
+    return (
+      <div>
+        <h1>Second Blog Page</h1>
+      </div>
+    );
+  }
+  
+  export default Second;
+  
