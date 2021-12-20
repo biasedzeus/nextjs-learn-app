052ea1e3347e1a5681fe35b455620b1ea62cbc80
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-20 12:46:06 +0530

    using router.push & router.replace to go to a route after an event

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import React from "react";
-
+import { useRouter } from "next/router";
 const Home = () => {
+  const router = useRouter();
+
+  function handleClick() {
+    console.log("placing order");
+    // router.push("/products/");
+    router.replace("/products/")
+  }
+
   return (
     <div>
       <h1>Home</h1>
@@ -14,6 +22,8 @@ const Home = () => {
       <Link href="/docs">
         <a>Docs</a>
       </Link>
+
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 };
